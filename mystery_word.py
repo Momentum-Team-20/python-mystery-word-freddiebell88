7787c4e71701ca8c50966916c0ef8336bfc5680c
freddiebell88 <frederikcwbell@gmail.com>
2023-11-02 19:53:28 -0400

    added response for multiple letter guess, need to not add it to word count

--- mystery_word.py
+++ mystery_word.py
@@ -1,8 +1,11 @@
 import random
 
 
-# def get_letter_guess():
-
+# def validate_guess(guess, guessed_letters):
+#     return (
+#         len(guess) >= 1
+#         # and guess in guessed_letters
+#     )
 
 def replace_letter(str):
     for character in str:
@@ -14,22 +17,26 @@ def play_game(file):
     with open(file) as reader:
         text = reader.read()
         word_list = text.split()
-        # print(word_list)
         answer = random.choice(word_list)
         print(answer)
         letters = [letter for letter in answer]
-        print(letters)
         game_board = [letter.replace(letter, '_') for letter in letters]
         print(game_board)
-        # game_board = "".join(blank_letters)
-        # print(game_board)
         guessed_letters = []
         guess_limit = 8
         wrong_guesses = 0
         while wrong_guesses < guess_limit:
-            guess = input("Guess a letter: ")
-            # print(guess)
+            guess = input("Guess a letter: ").lower()
             guessed_letters.append(guess)
+            # print(guessed_letters)
+            # if guessed_letters:
+            #     f
+            # for item in guessed_letters:
+            #         if guess == guessed_letters[item]:
+            #             print(f'You have already guessed {guess}.')
+            if len(guess) >= 1:
+                print("Invalid guess. Try again.")
+            # write code to not add this guess to word count
             if guess in answer:
                 print("correct!")
                 for index in range(len(answer)):
@@ -39,15 +46,10 @@ def play_game(file):
             else:
                 print("incorrect!")
                 wrong_guesses += 1
-                print(wrong_guesses)
+                print(f'You have used {wrong_guesses} of 8 guesses.')
             if "_" not in game_board:
                 print("VICTORY!")
                 break
-        # replace_letter()
-        # if guess in letters:
-        #     print(letter)
-        # else:
-        #     print('_')
 
 
 if __name__ == "__main__":
